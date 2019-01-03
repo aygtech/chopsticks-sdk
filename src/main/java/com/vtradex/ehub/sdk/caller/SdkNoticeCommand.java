642d42f03aa331b9639358b19e303e72fc22987b
zilong.li <zilong.li@56linked.com>
2019-01-03 10:38:49 +0800

    1.1.4 	1.新增traceNo功能 	2.新增extParams功能

--- src/main/java/com/vtradex/ehub/sdk/caller/SdkNoticeCommand.java
+++ src/main/java/com/vtradex/ehub/sdk/caller/SdkNoticeCommand.java
@@ -7,5 +7,14 @@ public class SdkNoticeCommand extends DefaultModernNoticeCommand {
 	public SdkNoticeCommand(String method, Object... params) {
 		super(method, params);
 	}
-
+	
+	public SdkNoticeCommand addTraceNo(String traceNo) {
+		getTraceNos().add(traceNo);
+		return this;
+	}
+	
+	public SdkNoticeCommand addExtParam(String key, String value) {
+		getExtParams().put(key, value);
+		return this;
+	}
 }
--- src/main/java/com/vtradex/ehub/sdk/handler/SdkContextHolder.java
+++ src/main/java/com/vtradex/ehub/sdk/handler/SdkContextHolder.java
@@ -3,5 +3,5 @@ package com.vtradex.ehub.sdk.handler;
 import com.chopsticks.core.rocketmq.modern.handler.ModernContextHolder;
 
 public class SdkContextHolder extends ModernContextHolder {
-
+	private SdkContextHolder() {}
 }
